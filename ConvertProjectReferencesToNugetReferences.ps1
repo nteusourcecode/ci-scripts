05bba9d48d004bf048cf25d75f2df62dabf325f1
joelnteu <30702475+joelnteu@users.noreply.github.com>
2017-08-16 15:59:17 -0400

    Update ConvertProjectReferencesToNugetReferences.ps1

--- ConvertProjectReferencesToNugetReferences.ps1
+++ ConvertProjectReferencesToNugetReferences.ps1
@@ -14,6 +14,20 @@ function Convert-ProjectReferences-To-NugetReferences ($projpath, $projname) {
 		# Remove each node from its parent
 		[void]$_.ParentNode.RemoveChild($_)
 	}
+	
+	$newcsItemGroup = $docCsproj.CreateElement("ItemGroup", $docCsproj.DocumentElement.NamespaceURI)
+	$newcsReference = $docCsproj.CreateElement("Reference", $docCsproj.DocumentElement.NamespaceURI)
+	$newcsReference.SetAttribute("Include","hellosupportl1, Version=1.0.0.0, Culture=neutral, processorArchitecture=MSIL");
+	$newcsHintPath = $docCsproj.CreateElement("HintPath", $docCsproj.DocumentElement.NamespaceURI)
+	$newcsHintPath.InnerXml = "..\packages\hellosupportl1.1.0.0\lib\net46\hellosupportl1.dll"
+	$newcsRefPrivate = $docCsproj.CreateElement("Private", $docCsproj.DocumentElement.NamespaceURI)
+	$newcsRefPrivate.InnerXml = "True"
+
+	$newcsReference.AppendChild($newcsHintPath)
+	$newcsReference.AppendChild($newcsRefPrivate)
+	$newcsItemGroup.AppendChild($newcsReference)
+	$docCsproj.Project.AppendChild($newcsItemGroup)	
+	
 	$docCsproj.Save($csproj)
 	
 	$slnProj = $projpath + '\' + $projname + '.sln'
@@ -21,4 +35,4 @@ function Convert-ProjectReferences-To-NugetReferences ($projpath, $projname) {
 	$lineNumberToDelete = $docSlnProj |Select-String -Pattern "hellosupportl1" -CaseSensitive | Select-Object LineNumber
 	$docSlnProj2 = $docSlnProj | Foreach {$n=1}{if (($n++) -ne ($lineNumberToDelete.LineNumber)) {$_}}
 	$docSlnProj2 | Foreach {$n=1}{if (($n++) -ne ($lineNumberToDelete.LineNumber)) {$_}} | Set-Content -Path $slnProj
-	}
+}
