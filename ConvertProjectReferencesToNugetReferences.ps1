3d57d97d7a3c2eb1fb2cc1a897560f5f1df459dd
joelnteu <30702475+joelnteu@users.noreply.github.com>
2017-08-17 10:10:19 -0400

    Update ConvertProjectReferencesToNugetReferences.ps1

--- ConvertProjectReferencesToNugetReferences.ps1
+++ ConvertProjectReferencesToNugetReferences.ps1
@@ -3,31 +3,55 @@ param (
     [string]$projname = $null
 )
 
-Write-Host "begin ConvertProjectReferencesToNugetReferences"
+#Files to modify
+$packagesConfig = $projpath + '\' + $projname +'\packages.config'
+$docPackagesConfig = (Get-Content $packagesConfig) -as [Xml]
 
-	$packagesConfig = $projpath + '\' + $projname +'\packages.config'
-	$docPackagesConfig = (Get-Content $packagesConfig) -as [Xml]
-	$newAppSetting = $docPackagesConfig.CreateElement("package")
+$packagesConfig = 'C:\Users\joel.mccune\Documents\temp\packages.config'
+$docPackagesConfig = (Get-Content $packagesConfig) -as [Xml]
+
+$slnProj = $projpath + '\' + $projname + '.sln'
+$docSlnProj = (Get-Content $slnProj)
+
+[System.Collections.ArrayList]$NugetPackagesToAdd = New-Object System.Collections.ArrayList
+
+#Find Project References
+$nugetprojToAdd = $docCsproj.Project.ItemGroup.ProjectReference | ForEach-Object {
+		$NugetPackagesToAdd.Add($_.Name)
+}
+
+Write-Host ("References found: " + $NugetPackagesToAdd)
+
+#Remove Existing References from the list of references to add
+$nugetprojToRemove = $docPackagesConfig.packages.package | ForEach-Object {
+		$NugetPackagesToAdd.Remove($_.id)
+}
+
+Write-Host ("Packages to add: " + $NugetPackagesToAdd)
+
+$NugetPackagesToAdd | ForEach-Object {
+	$currentPackageToAdd = $_
+
+	#BEGIN update packages.confg
+	$newAppSetting = $docPackagesConfig.CreateElement("package", $docPackagesConfig.DocumentElement.NamespaceURI)
 	$docPackagesConfig.packages.AppendChild($newAppSetting)
-	$newAppSetting.SetAttribute("id","hellosupportl1");
+	$newAppSetting.SetAttribute("id", $currentPackageToAdd);
 	$newAppSetting.SetAttribute("version","1.0.0");
 	$newAppSetting.SetAttribute("targetFramework","net46");
 	$docPackagesConfig.Save($packagesConfig)
-	Write-Host $packagesConfig
-	Write-Host $docPackagesConfig
 	
-	$csproj =  $projpath + '\' + $projname + '\' +$projname + '.csproj'
-	$docCsproj = (Get-Content $csproj) -as [Xml]
-	$csrefToRemove = $docCsproj.Project.ItemGroup.ProjectReference | Where-Object {$_.Name -eq "hellosupportl1" } | ForEach-Object {
+	#BEGIN update .csproj
+	$csrefToRemove = $docCsproj.Project.ItemGroup.ProjectReference | Where-Object {$_.Name -eq $currentPackageToAdd } | ForEach-Object {
 		# Remove each node from its parent
 		[void]$_.ParentNode.RemoveChild($_)
 	}
 	
+	#Add package reference
 	$newcsItemGroup = $docCsproj.CreateElement("ItemGroup", $docCsproj.DocumentElement.NamespaceURI)
 	$newcsReference = $docCsproj.CreateElement("Reference", $docCsproj.DocumentElement.NamespaceURI)
-	$newcsReference.SetAttribute("Include","hellosupportl1, Version=1.0.0.0, Culture=neutral, processorArchitecture=MSIL");
+	$newcsReference.SetAttribute("Include", $currentPackageToAdd + ", Version=1.0.0.0, Culture=neutral, processorArchitecture=MSIL");
 	$newcsHintPath = $docCsproj.CreateElement("HintPath", $docCsproj.DocumentElement.NamespaceURI)
-	$newcsHintPath.InnerXml = "..\packages\hellosupportl1.1.0.0\lib\net46\hellosupportl1.dll"
+	$newcsHintPath.InnerXml = "..\packages\" + $currentPackageToAdd + ".1.0.0\lib\net46\" + $currentPackageToAdd + ".dll"
 	$newcsRefPrivate = $docCsproj.CreateElement("Private", $docCsproj.DocumentElement.NamespaceURI)
 	$newcsRefPrivate.InnerXml = "True"
 
@@ -38,9 +62,8 @@ Write-Host "begin ConvertProjectReferencesToNugetReferences"
 	
 	$docCsproj.Save($csproj)
 	
-	$slnProj = $projpath + '\' + $projname + '.sln'
-	$docSlnProj = (Get-Content $slnProj)
-	$lineNumberToDelete = $docSlnProj |Select-String -Pattern "hellosupportl1" -CaseSensitive | Select-Object LineNumber
+	#BEGIN update .sln
+	$lineNumberToDelete = $docSlnProj |Select-String -Pattern $currentPackageToAdd -CaseSensitive | Select-Object LineNumber
 	$docSlnProj2 = $docSlnProj | Foreach {$n=1}{if (($n++) -ne ($lineNumberToDelete.LineNumber)) {$_}}
 	$docSlnProj2 | Foreach {$n=1}{if (($n++) -ne ($lineNumberToDelete.LineNumber)) {$_}} | Set-Content -Path $slnProj
-
+}
