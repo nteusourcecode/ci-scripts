da6c87cee60545df67e4cdc725e87fc79c4e0258
joelnteu <30702475+joelnteu@users.noreply.github.com>
2017-08-21 15:46:45 -0400

    Create GitPullAllRepos.ps1

---
+++ GitPullAllRepos.ps1
@@ -0,0 +1,9 @@
+$dirs = Get-ChildItem -Path . | Where-Object {$_.PSIsContainer -eq $True}
+$back = pwd
+foreach ($dir in $dirs)
+{
+    cd $dir.FullName
+    Write-Host $dir.FullName
+	git pull --all
+}   
+cd $back.Path
