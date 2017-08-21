fb0a1f95a4326049a7b07238500f7fc261d6c572
joelnteu <30702475+joelnteu@users.noreply.github.com>
2017-08-21 15:50:08 -0400

    Delete GitPullAllRepos.ps1

--- GitPullAllRepos.ps1
+++
@@ -1,9 +0,0 @@
-$dirs = Get-ChildItem -Path ../ | Where-Object {$_.PSIsContainer -eq $True}
-$back = pwd
-foreach ($dir in $dirs)
-{
-    cd $dir.FullName
-    Write-Host $dir.FullName
-	git pull --all
-}   
-cd $back.Path
