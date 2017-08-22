f7e6c750c65177a3107d757a61611fde97f30de0
joelnteu <30702475+joelnteu@users.noreply.github.com>
2017-08-22 14:04:04 -0400

    Update SetupNuspec.ps1

--- SetupNuspec.ps1
+++ SetupNuspec.ps1
@@ -9,6 +9,54 @@ $xml.package.metadata.version = $buildVersion
 $xml.package.metadata.authors = "NTEU Developer Team"
 $xml.package.metadata.owners = "NTEU"
 
+$nodes = $xml.package.ChildNodes
+$nodes | % {
+	$child_node = $_.SelectSingleNode('licenseUrl')
+	if($child_node)
+	{
+		$_.RemoveChild($child_node) | Out-Null
+	}
+	$child_node = $_.SelectSingleNode('projectUrl')
+	if($child_node)
+	{
+		$_.RemoveChild($child_node) | Out-Null
+	}
+	$child_node = $_.SelectSingleNode('iconUrl')
+	if($child_node)
+	{
+		$_.RemoveChild($child_node) | Out-Null
+	}
+	$child_node = $_.SelectSingleNode('requireLicenseAcceptance')
+	if($child_node)
+	{
+		$_.RemoveChild($child_node) | Out-Null
+	}
+	$child_node = $_.SelectSingleNode('description')
+	if($child_node)
+	{
+		$_.RemoveChild($child_node) | Out-Null
+	}
+	$child_node = $_.SelectSingleNode('releaseNotes')
+	if($child_node)
+	{
+		$_.RemoveChild($child_node) | Out-Null
+	}
+	$child_node = $_.SelectSingleNode('copyright')
+	if($child_node)
+	{
+		$_.RemoveChild($child_node) | Out-Null
+	}
+	$child_node = $_.SelectSingleNode('tags')
+	if($child_node)
+	{
+		$_.RemoveChild($child_node) | Out-Null
+	}
+	$child_node = $_.SelectSingleNode('dependencies')
+	if($child_node)
+	{
+		$_.RemoveChild($child_node) | Out-Null
+	}
+}
 
 Write-Host ($projectName +".nuspec has been updated.")
 Write-Host ($projectName +".nuspec version: " + $buildVersion)
