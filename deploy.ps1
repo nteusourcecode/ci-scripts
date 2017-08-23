6f22158c73f51bd3579edd97feb9d4d59c055e95
joelnteu <30702475+joelnteu@users.noreply.github.com>
2017-08-23 17:29:54 -0400

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,5 @@
 Import-Module WebAdministration #Make sure you have this module installed on your computer
 $AppPoolName = $env:APPPOOL_NAME
-Write-Host ("ABC" + $env:APPPOOL_NAME)
-#$AppPool = Get-Item IIS:\AppPools\$AppPoolName
-#$AppPool.startMode = "alwaysrunning"
-#$AppPool | Set-Item -Verbose
+$AppPool = Get-Item IIS:\AppPools\$AppPoolName
+$AppPool.startMode = "alwaysrunning"
+$AppPool | Set-Item -Verbose
