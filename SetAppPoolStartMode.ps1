a67412d0808b63b02091d9872465c4c96fb13787
joelnteu <30702475+joelnteu@users.noreply.github.com>
2017-08-23 12:29:49 -0400

    Update SetAppPoolStartMode.ps1

--- SetAppPoolStartMode.ps1
+++ SetAppPoolStartMode.ps1
@@ -1,5 +1,6 @@
 Import-Module WebAdministration #Make sure you have this module installed on your computer
 $AppPoolName = $env:apppool_name
-$AppPool = Get-Item IIS:\AppPools\$AppPoolName
-$AppPool.startMode = "alwaysrunning"
-$AppPool | Set-Item -Verbose
+Write-Host IIS:\AppPools\$AppPoolName
+#$AppPool = Get-Item IIS:\AppPools\$AppPoolName
+#$AppPool.startMode = "alwaysrunning"
+#$AppPool | Set-Item -Verbose
