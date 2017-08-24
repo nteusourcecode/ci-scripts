d987d2913c96247f80183d22ae6cf5982d244f5e
joelnteu <30702475+joelnteu@users.noreply.github.com>
2017-08-24 10:27:05 -0400

    Create AddDeployTarget.ps1

---
+++ AddDeployTarget.ps1
@@ -0,0 +1,32 @@
+$csProjPath = 'C:\projects\hello-world-p1\helloworldp1\helloworldp1.csproj'
+$deployps1Path = 'C:\projects\hello-world-p1\helloworldp1\deploy.ps1'
+
+if(!Test-Path $deployps1Path)
+{
+	Move-Item C:\ciscripts\deploy.ps1 C:\projects\hello-world-p1\helloworldp1
+}
+
+if(Test-Path $deployps1Path)
+{
+	# load it into an XML object:
+	$xml = New-Object -TypeName XML
+	$xml.Load($csProjPath)
+		
+   $hasDeployPowershell = $xml.Project.Target | Where-Object {$_.Name -eq 'BeforeBuild'} | Select-Object -Property {$_.ItemGroup.Content.Include}
+
+	if(!($hasDeployPowershell.'$_.ItemGroup.Content.Include' -eq 'deploy.ps1'))
+	{
+		$target = $xml.CreateElement("Target", $xmlDoc.DocumentElement.NamespaceURI)
+		$target.SetAttribute("Name", "BeforeBuild")
+		$itemGroup = $xml.CreateElement("ItemGroup", $xmlDoc.DocumentElement.NamespaceURI)
+		$content = $xml.CreateElement("Content", $xmlDoc.DocumentElement.NamespaceURI)
+		$content.SetAttribute("Include", "deploy.ps1")
+		$itemGroup.AppendChild($content)
+		$target.AppendChild($itemGroup)
+		$xml.Project.AppendChild($target)
+
+		$xml.Save($csProjPath)
+
+		Write-Host "deploy.ps1 was added as a target to the .csproj file"
+	}
+}
