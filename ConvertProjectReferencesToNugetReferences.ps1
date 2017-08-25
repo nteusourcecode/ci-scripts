c48694a2769bff6f2875cf504a322e82db13663d
Joel McCune <30702475+joelnteu@users.noreply.github.com>
2017-08-25 17:45:38 -0400

    Update ConvertProjectReferencesToNugetReferences.ps1

--- ConvertProjectReferencesToNugetReferences.ps1
+++ ConvertProjectReferencesToNugetReferences.ps1
@@ -91,7 +91,7 @@ $NugetPackagesToAdd | ForEach-Object {
 	$newcsReference.AppendChild($newcsRefPrivate)
 	$newcsItemGroup.AppendChild($newcsReference)
 	$docCsproj.Project.AppendChild($newcsItemGroup)	
-	Write-Host $newcsItemGroup.ToString()
+	Write-Host Format-Xml -InputObject $newcsItemGroup
 	$docCsproj.Save($csproj)
 	
 	#BEGIN update .sln
