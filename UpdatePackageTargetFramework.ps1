925ad8efb4eafbf6784889fc76966f7a82ea7326
Joel McCune <30702475+joelnteu@users.noreply.github.com>
2017-08-28 17:52:34 -0400

    Update UpdatePackageTargetFramework.ps1

--- UpdatePackageTargetFramework.ps1
+++ UpdatePackageTargetFramework.ps1
@@ -3,12 +3,14 @@ if(Test-Path $env:NTEU_PACKAGES_PATH)
 	$xmlDoc = [xml](Get-Content $env:NTEU_PACKAGES_PATH);
 	if($xmlDoc.NTEUPackages.HasChildNodes)
 	{
-		$xmlDoc.NTEUPackages.Package |  ForEach-Object {
-      $framework = Get-ChildItem $env:PACKAGES_PATH + $_.id + "." + $_.version +"\lib" | Sort-Object Name -descending | Select-Object Name -First 1
-      $nodeToUpdate = $xmlDoc.packages.SelectSingleNode("package[@id='$($framework.Name)']")
-      $nodeToUpdate.SetAttribute("targetFramework", $framework.Name)
-      $xmlDoc.Save($XML_Path)
-      Write-Host "Set package $($_.id) target framework to $($framework.Name)"
-	  }
+	    $xmlDoc.NTEUPackages.Package |  ForEach-Object {
+      		$framework = Get-ChildItem $env:PACKAGES_PATH + $_.id + "." + $_.version +"\lib" | Sort-Object Name -descending | Select-Object Name -First 1
+      		
+		$xmlDoc = [xml](Get-Content $env:PROJECT_PACKAGES_PATH);
+		$nodeToUpdate = $xmlDoc.packages.SelectSingleNode("package[@id='$($framework.Name)']")
+      		$nodeToUpdate.SetAttribute("targetFramework", $framework.Name)
+      		$xmlDoc.Save($XML_Path)
+      		Write-Host "Update package $($_.id) target framework to $($framework.Name)"
+	    }
 	}
 }
