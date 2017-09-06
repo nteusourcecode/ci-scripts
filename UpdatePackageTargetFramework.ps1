a575c6d41fc15051ac94360229d3f05112141a5f
Joel McCune <30702475+joelnteu@users.noreply.github.com>
2017-09-06 10:13:27 -0400

    Update UpdatePackageTargetFramework.ps1

--- UpdatePackageTargetFramework.ps1
+++ UpdatePackageTargetFramework.ps1
@@ -4,7 +4,7 @@ if(Test-Path $env:NTEU_PACKAGES_PATH)
 	if($xmlDoc.NTEUPackages.HasChildNodes)
 	{
 	    $xmlDoc.NTEUPackages.Package |  ForEach-Object {
-	       Write-Host ("PackagesPath: " + "$($env:PACKAGES_PATH)$($_.id).$($_.version)\lib")
+	        Write-Host ("PackagesPath: " + "$($env:PACKAGES_PATH)$($_.id).$($_.version)\lib")
       		$framework = Get-ChildItem "$($env:PACKAGES_PATH)$($_.id).$($_.version)\lib" | Sort-Object Name -descending | Select-Object Name -First 1
       		
 		$xmlDocProjectConfig = [xml](Get-Content $env:PROJECT_PACKAGES_PATH);
