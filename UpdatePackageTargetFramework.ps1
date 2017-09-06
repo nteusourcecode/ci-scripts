915a145d99a491fe06fc6fd05627b9e43207bd9b
Joel McCune <30702475+joelnteu@users.noreply.github.com>
2017-09-06 12:36:42 -0400

    Update UpdatePackageTargetFramework.ps1

--- UpdatePackageTargetFramework.ps1
+++ UpdatePackageTargetFramework.ps1
@@ -12,6 +12,16 @@ if(Test-Path $env:NTEU_PACKAGES_PATH)
       		$nodeToUpdate.SetAttribute("targetFramework", $framework.Name)
       		$xmlDocProjectConfig.Save($env:PROJECT_PACKAGES_PATH)
 		
+      		Write-Output "Update package $($_.id) target framework to $($framework.Name)"
+	    }
+	}
+	
+	if($xmlDoc.NTEUPackages.HasChildNodes)
+	{
+	    $xmlDoc.NTEUPackages.Package |  ForEach-Object {
+	        Write-Host ("cs proj PackagesPath: " + "$($env:PACKAGES_PATH)$($_.id).$($_.version)\lib")
+      		$framework = Get-ChildItem "$($env:PACKAGES_PATH)$($_.id).$($_.version)\lib" | Sort-Object Name -descending | Select-Object Name -First 1
+      				
 		$csProj = $env:PROJECT_CSPROJ_PATH
 		$docCsproj = (Get-Content $csProj) -as [Xml]
 		Get-Content $csProj
@@ -19,7 +29,7 @@ if(Test-Path $env:NTEU_PACKAGES_PATH)
 		Write-Output $docCsproj.Project.ItemGroup
 		Write-Output 'Reference:'
 		Write-Output $docCsproj.Project.ItemGroup.Reference
-		Write-Output ('docCsproj.Project.ItemGroup.Reference.HintPath: for' + "$($_.id).$($_.version):")
+		Write-Output ('docCsproj.Project.ItemGroup.Reference.HintPath: for' + '$($_.id).$($_.version):')
 		Write-Output $docCsproj.Project.ItemGroup.Reference.HintPath		
 		$projectToSetHitPath = $docCsproj.Project.ItemGroup.Reference | Where-Object {$_.HintPath -eq '$($_.id).$($_.version)' }		
 		Write-Output $projectToSetHitPath
