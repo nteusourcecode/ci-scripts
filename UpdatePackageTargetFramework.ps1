c665e82edbb2309d640229bb05b7e1195125dfcf
Joel McCune <30702475+joelnteu@users.noreply.github.com>
2017-09-06 11:44:32 -0400

    Update UpdatePackageTargetFramework.ps1

--- UpdatePackageTargetFramework.ps1
+++ UpdatePackageTargetFramework.ps1
@@ -14,6 +14,7 @@ if(Test-Path $env:NTEU_PACKAGES_PATH)
 		
 		$csProj = $env:PROJECT_CSPROJ_PATH
 		$docCsproj = (Get-Content $csProj) -as [Xml]
+		Get-Content $csProj
 		$projectToSetHitPath = $docCsproj.Project.ItemGroup.Reference | Where-Object {$_.HintPath -eq "$($_.id).$($_.version)" }
 		$projectToSetHitPath.HintPath = "$($env:PACKAGES_PATH)$($_.id).$($_.version)\lib\$($framework.Name)\$($_.id).dll"
 		$docCsproj.Save($csProj)
