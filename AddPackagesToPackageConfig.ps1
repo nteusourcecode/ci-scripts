e93d0d10a451860e8a278e6aba609b67c826c957
Joel McCune <30702475+joelnteu@users.noreply.github.com>
2017-10-02 13:23:17 -0400

    Create AddPackagesToPackageConfig.ps1

---
+++ AddPackagesToPackageConfig.ps1
@@ -0,0 +1,46 @@
+$packagesPath = $env:PACKAGES_PATH
+$packagesConfig = $env:PROJECT_PACKAGES_PATH
+$xmlDoc = [xml](Get-Content $packagesConfig);
+
+$packageNames = Get-ChildItem $packagesPath | Where-Object Name -NotMatch '^Microsoft.' | Select-Object Name
+#Write-Output $packageNames
+
+foreach($i in $packageNames[0])
+{
+	Write-Output $i
+	$currentLibPath = "$($packagesPath)\$($i.Name)\lib"
+	$framework = ""
+
+	if(Test-Path $currentLibPath)
+	{
+		$framework = Get-ChildItem $currentLibPath | Where-Object Name -Match '^net[0-9][0-9]' | Sort-Object Name -descending | Select-Object Name -First 1
+		if($framework.Length -ge 1 -and !$framework.Name.StartsWith("net"))
+		{
+			$framework.Name = ""
+		}
+	}
+	
+	if([string]::IsNullOrEmpty($framework.Name))
+	{
+		$framework = "net45"
+	}
+
+	Write-Output $i.Name.IndexOf(".")
+	$indexStartOfVersion = $i.Name.IndexOf(".")
+	$packageVersion = $i.Name.Substring($indexStartOfVersion + 1)
+	$packageName = $i.Name.Substring(0, $indexStartOfVersion)
+
+	Write-Output $framework
+
+	$addPackage = $xmlDoc.packages.SelectSingleNode("package[@id='$($packageName)']").Count -eq 0
+	if($addPackage)
+	{
+		Write-Output "adding"
+		$newPackageNode = $xmlDoc.CreateElement("package", $xmlDoc.DocumentElement.NamespaceURI)
+		$newPackageNode.SetAttribute("id", $packageName)
+		$newPackageNode.SetAttribute("version", $packageVersion)
+		$newPackageNode.SetAttribute("targetFramework", $framework)
+		$xmlDoc.packages.AppendChild($newPackageNode)
+		$xmlDoc.Save($packagesConfig)
+	}
+}
