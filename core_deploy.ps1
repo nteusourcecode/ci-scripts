4ffb3a5c2af6011cf4fcbc6792be1697fd279b07
Joel McCune <30702475+joelnteu@users.noreply.github.com>
2017-11-16 11:23:02 -0500

    Create core_deploy.ps1

---
+++ core_deploy.ps1
@@ -0,0 +1,7 @@
+#Set app pool to always running
+Import-Module WebAdministration 
+$AppPoolName = $env:APPPOOL_NAME
+$AppPool = Get-Item IIS:\AppPools\$AppPoolName
+$AppPool.startMode = "alwaysrunning"
+$AppPool.managedRuntimeVersion = ""
+$AppPool | Set-Item -Verbose
