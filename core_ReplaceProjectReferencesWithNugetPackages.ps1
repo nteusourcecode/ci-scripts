c51141b1d2cd4d36f1fb7a323bfc6c359ebd66ef
Joel McCune <30702475+joelnteu@users.noreply.github.com>
2017-11-21 12:37:35 -0500

    Create core_ReplaceProjectReferencesWithNugetPackages.ps1

---
+++ core_ReplaceProjectReferencesWithNugetPackages.ps1
@@ -0,0 +1,21 @@
+$csProjPath = $env:PROJECT_PATH
+
+[System.Collections.ArrayList]$ReferencesFound = New-Object System.Collections.ArrayList
+[System.Collections.ArrayList]$AppVeyorPackageName = New-Object System.Collections.ArrayList
+
+$ReferencesFound.AddRange((dotnet list $csProjPath reference | select-object -skip 2))
+
+$AppVeyorPackageName.AddRange((nuget list -source appveyor | select-object -skip 1 | ForEach-Object -Process {([String] $_).Split(" ")[0]}))
+
+$ReferencesFound | ForEach-Object {
+	if($AppVeyorPackageName -icontains [System.IO.Path]::GetFileNameWithoutExtension($_))
+	{
+		dotnet remove $csProjPath reference $_
+		dotnet add $csProjPath package [System.IO.Path]::GetFileNameWithoutExtension($_)
+		Write-Output "Replaced .csproj reference with appveyor nuget package: $($([System.IO.Path]::GetFileNameWithoutExtension($_)))"		
+	}
+	else
+	{
+		Write-Output "Appveyor nuget package not found expecting .csproj reference: $($_)"
+	}
+}
