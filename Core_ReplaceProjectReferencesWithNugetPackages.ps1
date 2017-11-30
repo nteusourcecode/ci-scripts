91d84715cb452e17c5fb41e0d6ecad49adedd2de
Joel McCune <30702475+joelnteu@users.noreply.github.com>
2017-11-30 17:36:48 -0500

    Update Core_ReplaceProjectReferencesWithNugetPackages.ps1

--- Core_ReplaceProjectReferencesWithNugetPackages.ps1
+++ Core_ReplaceProjectReferencesWithNugetPackages.ps1
@@ -3,22 +3,29 @@ $csProjPath = $env:PROJECT_PATH
 [System.Collections.ArrayList]$ReferencesFound = New-Object System.Collections.ArrayList
 [System.Collections.ArrayList]$AppVeyorPackageName = New-Object System.Collections.ArrayList
 
-Write-Output (-not ([string]::IsNullOrEmpty(dotnet list $csProjPath reference))) 
+$objectinfo = (dotnet list $csProjPath reference | measure-object -Line)
 
-$ReferencesFound.AddRange((dotnet list $csProjPath reference | select-object -skip 2))
+if(($objectinfo.Lines -eq 1))
+{
+	$ReferencesFound.AddRange((dotnet list $csProjPath reference | select-object -skip 2))
 
-$AppVeyorPackageName.AddRange((nuget list -source AppVeyorAccountFeed | select-object -skip 1 | ForEach-Object -Process {([String] $_).Split(" ")[0]}))
+	$AppVeyorPackageName.AddRange((nuget list -source AppVeyorAccountFeed | select-object -skip 1 | ForEach-Object -Process {([String] $_).Split(" ")[0]}))
 
-$ReferencesFound | ForEach-Object {
-	$CurrentReference = $_
-	if($AppVeyorPackageName -icontains [System.IO.Path]::GetFileNameWithoutExtension($CurrentReference))
-	{
-		dotnet remove $csProjPath reference $CurrentReference
-		dotnet add $csProjPath package ([System.IO.Path]::GetFileNameWithoutExtension($CurrentReference)) -s AppVeyorAccountFeed -n
-		Write-Output "Replaced .csproj reference with appveyor nuget package: $($([System.IO.Path]::GetFileNameWithoutExtension($CurrentReference)))"		
+	$ReferencesFound | ForEach-Object {
+		$CurrentReference = $_
+		if($AppVeyorPackageName -icontains [System.IO.Path]::GetFileNameWithoutExtension($CurrentReference))
+		{
+			dotnet remove $csProjPath reference $CurrentReference
+			dotnet add $csProjPath package ([System.IO.Path]::GetFileNameWithoutExtension($CurrentReference)) -s AppVeyorAccountFeed -n
+			Write-Output "Replaced .csproj reference with appveyor nuget package: $($([System.IO.Path]::GetFileNameWithoutExtension($CurrentReference)))"		
+		}
+		else
+		{
+			Write-Output "Appveyor nuget package not found expecting .csproj reference: $($CurrentReference)"
+		}
 	}
-	else
-	{
-		Write-Output "Appveyor nuget package not found expecting .csproj reference: $($CurrentReference)"
-	}
+}
+else
+{
+	Write-Output "This project does not contain any references."
 }
