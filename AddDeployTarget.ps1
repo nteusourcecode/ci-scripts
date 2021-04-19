5feaa4730cec657de4d78545372fa885d375f695
Matthew Harbour <matthew.harbour@nteu.org>
2021-04-19 15:23:29 -0400

    change direction of slash

--- AddDeployTarget.ps1
+++ AddDeployTarget.ps1
@@ -32,5 +32,5 @@ if(Test-Path $deployps1Path)
 }
 
 # Create build info file
-New-Item "$env:PROJECT_PATH/NteuAppBuild.txt" -Value "APPPOOL_NAME=$env:APPPOOL_NAME\nBUILD_DATE=$env:APPVEYOR_REPO_COMMIT_TIMESTAMP\nBUILD_VERSION=$env:APPVEYOR_BUILD_VERSION"
+New-Item "$env:PROJECT_PATH\NteuAppBuild.txt" -Value "APPPOOL_NAME=$env:APPPOOL_NAME\nBUILD_DATE=$env:APPVEYOR_REPO_COMMIT_TIMESTAMP\nBUILD_VERSION=$env:APPVEYOR_BUILD_VERSION"
 Write-Host "Build info file created"
