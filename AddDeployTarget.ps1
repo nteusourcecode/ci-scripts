46828ed207d80b85e6bdc394cd32051b767a5d1e
Matthew Harbour <matthew.harbour@nteu.org>
2021-04-21 17:48:48 -0400

    change path creation testing implementation

--- AddDeployTarget.ps1
+++ AddDeployTarget.ps1
@@ -34,8 +34,7 @@ if(Test-Path $deployps1Path)
 # Create build info file
 $buildInfoPath = "$($env:PROJECT_PATH)\NteuAppBuild.txt"
 New-Item -path $buildInfoPath -type "file" -Force -Value "APPPOOL_NAME=$env:APPPOOL_NAME\nBUILD_DATE=$env:APPVEYOR_REPO_COMMIT_TIMESTAMP\nBUILD_VERSION=$env:APPVEYOR_BUILD_VERSION"
-Write-Host "Path created"
-Write-Output $buildInfoPath | Test-Path
+if (Test-Path $buildInfoPath) {Write-Host "Path created"} else {Write-Host "Path not created"}
 Write-Host "Build info content:\n"
 Get-Content $buildInfoPath
 Write-Host "Build info file created"
