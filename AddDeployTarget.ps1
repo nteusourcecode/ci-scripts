9dc71a07a7e888e59c904760068c9fecd8d0ee40
Matthew Harbour <matthew.harbour@nteu.org>
2021-04-21 17:43:10 -0400

    add output

--- AddDeployTarget.ps1
+++ AddDeployTarget.ps1
@@ -35,7 +35,7 @@ if(Test-Path $deployps1Path)
 $buildInfoPath = "$($env:PROJECT_PATH)\NteuAppBuild.txt"
 New-Item -path $buildInfoPath -type "file" -Force -Value "APPPOOL_NAME=$env:APPPOOL_NAME\nBUILD_DATE=$env:APPVEYOR_REPO_COMMIT_TIMESTAMP\nBUILD_VERSION=$env:APPVEYOR_BUILD_VERSION"
 Write-Host "Path created"
-Test-Path $buildInfoPath
+Write-Output $buildInfoPath | Test-Path
 Write-Host "Build info content:\n"
 Get-Content $buildInfoPath
 Write-Host "Build info file created"
